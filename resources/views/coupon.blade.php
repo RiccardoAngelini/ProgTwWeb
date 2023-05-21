57229caf4ce11b9dfc640f1084535f9eaac47843
rlluca <lucarenzi301@gmail.com>
2023-05-21 16:03:55 +0200

    modifica pagina coupon

--- resources/views/coupon.blade.php
+++ resources/views/coupon.blade.php
@@ -1,13 +1,23 @@
 @extends('layouts.public')
 @section('title', 'Coupon')
 @section('content')
-<div class="tutto">
-<div class="con">
+<div class="content1">
+<div class="content2">
+<div class="text">
 Coupon
-<p> identificaote coupon: {{ $scelta_coupon->coupon_Id}}
-<p>Data di emissione:{{ date('d/m/Y', strtotime($scelta_coupon->date_emiss)) }}</p>
-<p>Data di scadenza:{{ date('d/m/Y', strtotime($scelta_coupon->date_exp)) }}</p>
-usufruibile sia in negozio che online 
+      </div>
+      <div class="field_coupon">
+          Identificaote coupon: {{ $scelta_coupon->coupon_Id}}
+</div>
+<div class="field_coupon">
+Data di emissione: {{ date('d/m/Y', strtotime($scelta_coupon->date_emiss)) }}
+</div>
+<div class="field_coupon">
+Data di scadenza: {{ date('d/m/Y', strtotime($scelta_coupon->date_exp)) }}
+</div>
+<div class="field_coupon">
+    <p>il buono è usufruibile sia in negozio che online </p>
+</div>
 </div>
 </div>
 @endsection
