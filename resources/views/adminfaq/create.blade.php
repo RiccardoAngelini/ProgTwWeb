f9991eede434e3d5b106499c34934aa86a6290c9
niccolociotti <niccolo.ciotti7@gmail.com>
2023-05-23 18:44:31 +0200

    cambiato form in laravel collective

--- resources/views/adminfaq/create.blade.php
+++ resources/views/adminfaq/create.blade.php
@@ -1,85 +1,38 @@
 @extends('layouts.admin')
 @section('content')
-   <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-        .faq-form {
-            margin-top:7em;
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 30px;
-            border: 1px solid #ccc;
-            background-color: #f9f9f9;
-        }
-        .faq-form h2 {
-            margin-top: 0;
-        }
-        .faq-form .form-group {
-            margin-bottom: 20px;
-        }
-        .faq-form label {
-            display: block;
-            font-weight: bold;
-        }
-        .faq-form input[type="text"],
-        .faq-form textarea {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            resize: vertical;
-        }
-        .faq-form button {
-            padding: 10px 20px;
-            background-color: #4CAF50;
-            color: #fff;
-            border: none;
-            cursor: pointer;
-        }
-        .faq-form button:hover {
-            background-color: #45a049;
-            
-        }
-        .btn a{
-        background-color: #2854e3;
-        border: none;
-        color: white;
-        padding: 5px 20px; 
-        text-align: center;
-        text-decoration: none;
-        font-size: 17px;
-        margin: 0.5px 0px;
-        cursor: pointer;
-        style="margin-left: 17%
-    }
-    </style> 
 
 <div class="faq-header"></div>
-<div class="btn" style="text-align: center; ;margin-top: 50px;"><a href="{{route('faq.index')}}">Back</a></div>
+<div class="btn" style="text-align: center; ;margin-top: 50px;"><a href="{{route('faq.index')}}">Indietro</a></div>
 <div class="title" style="margin-top: 50px;">
     <table><h1 style="text-align: center; font-size:50px;">Aggiungi Faq</h1>
 </div>
 
     <h2 style="margin-left: 25%; margin-top:70px;">inserisci la domanda</h2>
     <div class="faq-form" style="margin-top: 30px;">
-        <form  role="form" action="{{route('adminfaq.store')}}" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="question">Question :</label>
-                <input type="text" id="question" name="question" placeholder="Inserire la domanda" class="form-control @error('question') is-invalid @enderror" value="{{old('question')}}" >
-                    @error('question')
-                        <p class="invalid-feedback">{{$message}}</p>
-                    @enderror
-            </div>
-            <div class="form-group">
-                <label for="answer">Answer :</label>
-                <textarea id="answer" name="answer" rows="4" placeholder="Inserire una risposta" class="form-control @error('answer') is-invalid @enderror" >{{old('answer')}}</textarea>
-                    @error('answer')
-                        <div class="invalid-feedback">{{$message}}</div>
-                    @enderror
-            </div>
-            <button type="submit">Agguingi</button>
-        </form>
+    {{ Form::open(['route' => 'adminfaq.store', 'role' => 'form']) }}
+    {{ csrf_field() }}
+
+    <div class="form-group">
+        {{ Form::label('question', 'Domanda :',['class' => 'details' ]) }}
+        {{ Form::text('question', old('question'), ['id' => 'question', 'placeholder' => 'Inserire la domanda']) }}
+        <ul class="errors">
+                    @foreach ($errors->get('question') as $message)
+                    <li>{{ $message }}</li>
+                    @endforeach
+                </ul>
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('answer', 'Risposta :',['class' => 'details' ]) }}
+        {{ Form::textarea('answer', old('answer'), ['id' => 'answer', 'rows' => 4, 'placeholder' => 'Inserire una risposta' ]) }}
+<ul class="errors">
+                    @foreach ($errors->get('answer') as $message)
+                    <li>{{ $message }}</li>
+                    @endforeach
+                </ul>    </div>
+
+    {{ Form::submit('Aggiungi', ['class' => 'button-login']) }}
+{{ Form::close() }}
+
     </div>
 @endsection
--- resources/views/adminfaq/edit.blade.php
+++ resources/views/adminfaq/edit.blade.php
@@ -8,80 +8,24 @@
 
     <h2 style="margin-left: 25%; margin-top:70px;">inserisci la domanda</h2>
     <div class="faq-form" style="margin-top: 30px;">
-        <form  role="form" action="{{route('adminfaq.update', $faq -> id)}}" method="POST">
-            @csrf
-            @method('put')
-            <div class="form-group">
-                <label for="question">Question :</label>
-                <input type="text" id="question" name="question" placeholder="Inserire la domanda" class="form-control @error('question') is-invalid @enderror" value="{{old('question', $faq -> question)}}" >
-                    @error('question')
-                        <p class="invalid-feedback">{{$message}}</p>
-                    @enderror
-            </div>
-            <div class="form-group">
-                <label for="answer">Answer :</label>
-                <textarea id="answer" name="answer" rows="4" placeholder="Inserire una risposta" class="form-control @error('answer') is-invalid @enderror" >{{old('answer', $faq -> answer)}}</textarea>
-                    @error('answer')
-                        <div class="invalid-feedback">{{$message}}</div>
-                    @enderror
-            </div>
-            <button type="submit">Salva</button>
-        </form>
-    </div>
-   <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-        .faq-form {
-            margin-top:7em;
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 30px;
-            border: 1px solid #ccc;
-            background-color: #f9f9f9;
-        }
-        .faq-form h2 {
-            margin-top: 0;
-        }
-        .faq-form .form-group {
-            margin-bottom: 20px;
-        }
-        .faq-form label {
-            display: block;
-            font-weight: bold;
-        }
-        .faq-form input[type="text"],
-        .faq-form textarea {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            resize: vertical;
-        }
-        .faq-form button {
-            padding: 10px 20px;
-            background-color: #4CAF50;
-            color: #fff;
-            border: none;
-            cursor: pointer;
-        }
-        .faq-form button:hover {
-            background-color: #45a049;
-            
-        }
-        .btn a{
-        background-color: #2854e3;
-        border: none;
-        color: white;
-        padding: 5px 20px; 
-        text-align: center;
-        text-decoration: none;
-        font-size: 17px;
-        margin: 0.5px 0px;
-        cursor: pointer;
-        style="margin-left: 17%
-    }
-    </style> 
+    {{ Form::open(['route' => ['adminfaq.update', $faq->id], 'method' => 'PUT', 'role' => 'form']) }}
+    {{ csrf_field() }}
 
+    <div class="form-group">
+        {{ Form::label('question', 'Question :') }}
+        {{ Form::text('question', old('question', $faq->question), ['id' => 'question', 'placeholder' => 'Inserire la domanda', 'class' => 'form-control' . ($errors->has('question') ? ' is-invalid' : '')]) }}
+        {{ $errors->first('question', '<p class="invalid-feedback">:message</p>') }}
+    </div>
+
+    <div class="form-group">
+        {{ Form::label('answer', 'Answer :') }}
+        {{ Form::textarea('answer', old('answer', $faq->answer), ['id' => 'answer', 'rows' => 4, 'placeholder' => 'Inserire una risposta', 'class' => 'form-control' . ($errors->has('answer') ? ' is-invalid' : '')]) }}
+        {{ $errors->first('answer', '<div class="invalid-feedback">:message</div>') }}
+    </div>
+
+    {{ Form::submit('Salva', ['class' => 'button-login']) }}
+    {{ Form::close() }}
+
+    </div>
 
 @endsection
