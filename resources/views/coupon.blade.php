a30bb42b07dd8ae72a9f1f79d0e2735827b75bdc
niccolociotti <niccolo.ciotti7@gmail.com>
2023-05-28 17:13:04 +0200

    modificato pagina coupon

--- resources/views/coupon.blade.php
+++ resources/views/coupon.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="{{ asset('css/style.css')}}">
     <link rel="stylesheet" href="{{ asset('css/staff.css')}}">
 </head>
-<body>
+<body class="coupon">
 
 @if (session('success'))
     <div class="alert alert-success">
@@ -26,9 +26,6 @@
 <button class="button-back2"><a class="btn2" href="{{route('catalogo')}}">Torna al catalogo</a></button>
 <div class="content1">
 <div class="content2">
-<div class="text">
-
-      </div>
       <div class="info">
       <div class="field_coupon">
         @foreach($coupon as $coup)
@@ -36,17 +33,12 @@
       @endforeach
 </div>
 <div class="field_coupon">
-
-</div>
-<div class="field_coupon">
-
-</div>
-<div class="field_coupon">
     <p>Il buono è usufruibile sia in negozio che online </p>
 </div>
 
-</div>
+
 <div class="stampa"><a href="">Stampa</a>
+</div>
 </div>
 </div>
 </div>
