981dc49d3324ad433937ef33b557dd532990895a
rlluca <lucarenzi301@gmail.com>
2023-05-29 11:36:02 +0200

    modifica css

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -6,48 +6,11 @@
 
             <div class="contatti_indirizzo_nome">Indirizzo: Via Brecce Bianche, 12, 60131 Ancona </div>
             <div class="contatti_indirizzo_numero">Telefono: 071 220 4708 </div>
-</div>
+
         <div class="riga"></div>
-        <div class="contatti">
             <p class="contatti_modulo">
                 Nella sezione FAQ puoi trovare più velocemente la risposta alle tue domande. 
-                Se ci stai scrivendo perché hai riscontrato dei problemi con un'offerta o un codice sconto, ricordati di specificare il titolo o di indicare l'url dell'offerta.
-                <h2 class="contatti_compila"> Compila il modulo </h2>
+                Se ci stai contattando perché hai riscontrato dei problemi con un'offerta o un codice sconto, ricordati di specificare il titolo o di indicare l'url dell'offerta.
             </p>
-
-<div class="container2">
-            <div class="container1">
-                       <form action="/action_page.php">
-                    <div class="row">
-                        <div class="col-25">
-                            <label for="fname">Nome</label>
-                        </div>
-                        <div class="col-75">
-                            <input type="text" class="modulo" id="nome" name="nome" placeholder="Your name..">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-25">
-                            <label for="email">Email</label>
-                        </div>
-                        <div class="col-75">
-                            <input type="text" class="modulo" id="email" name="email" placeholder="Your mail">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-25">
-                            <label for="massage">Messaggio</label>
-                        </div>
-                        <div class="col-75">
-                            <textarea id="massage" name="massage" placeholder="Inserisci il messaggio" style="height:200px"></textarea>
-                        </div>
-                    </div>
-                    <br>
-                    <div class="row">
-                    <input class="submit-button-con" type="submit" value="Invia">
-                    </div>
-                </form>
-                </div>
-</div>
 </div>
 @endsection
