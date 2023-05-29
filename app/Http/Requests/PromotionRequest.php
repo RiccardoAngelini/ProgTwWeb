e570284b7df7e350b784e08a2960204e761c3639
RiccardoAngelini <rikyange@gmail.com>
2023-05-29 17:16:33 +0200

    ajax aggiungi una promozione
    ajax aggiungi una promozione

--- app/Http/Requests/PromotionRequest.php
+++ app/Http/Requests/PromotionRequest.php
@@ -4,6 +4,11 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+
 class PromotionRequest extends FormRequest
 {
     /**
@@ -34,4 +39,14 @@ class PromotionRequest extends FormRequest
             'image'=> 'required',
         ];
     }
+
+
+        /**
+     * Override: response in formato JSON
+    */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
 }
